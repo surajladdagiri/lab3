52b4cbe95172484ec87f76b7fecd4d28114438c8
surajladdagiri <152471106+surajladdagiri@users.noreply.github.com>
2024-02-13 19:06:26 -0800

    Fix Array

--- ArrayTests.java
+++ ArrayTests.java
@@ -7,6 +7,10 @@ public class ArrayTests {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+
+    int[] input2 = {1,2,3};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{3,2,1},input2);
 	}
 
 
@@ -14,5 +18,10 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+
+    int[] input2 = {1,2,3};
+    int[] result = ArrayExamples.reversed(input2);
+    assertArrayEquals(new int[]{3,2,1}, result);
+    assertArrayEquals(new int[]{1,2,3}, input2);
   }
 }
